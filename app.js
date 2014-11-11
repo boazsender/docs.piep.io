6f30882119e828bb39f2f98802642fda4c37a758
ashley williams <ashley@bocoup.com>
2014-11-11 18:45:49 +0100

    break deploy into deploy and contrib, make contrib a single page, update nav and routes

--- app.js
+++ app.js
@@ -11,19 +11,26 @@ app.set('view engine', 'jade');
 
 app.use(express.static(__dirname + '/public'));
 
-app.use('/deploy/:page', function(req, res){
-  var page = req.params.page;
-  res.render(page);
-});
-
 app.use('/endpoints/:endpoint', function(req, res){
   var endpoint = req.params.endpoint;
   var endpoint_data = yaml.safeLoad(fs.readFileSync('./data/'+ endpoint +'.yml'));
   res.render('endpoint', { resource_name: endpoint, endpoints: endpoint_data });
 });
 
+app.use('/endpoints', function(req,res){
+  res.render('endpoints');
+});
+
 app.use('/overview', function(req,res){
   res.render('overview');
+});
+
+app.use('/contrib', function(req, res){
+  res.render('contrib');
+});
+
+app.use('/deploy', function(req, res){
+  res.render('deploy');
 });
 
 app.use('/', function(req,res){
