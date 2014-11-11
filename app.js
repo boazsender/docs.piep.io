c41f05434a9ba9f3d96e818c3e9857e5f54cea75
ashley williams <ashley@bocoup.com>
2014-11-11 14:43:57 +0100

    basic routes, views, styles

--- app.js
+++ app.js
@@ -11,18 +11,23 @@ app.set('view engine', 'jade');
 
 app.use(express.static(__dirname + '/public'));
 
+app.use('/deploy/:page', function(req, res){
+  var page = req.params.page;
+  res.render(page);
+});
+
 app.use('/endpoints/:endpoint', function(req, res){
   var endpoint = req.params.endpoint;
   var endpoint_data = yaml.safeLoad(fs.readFileSync('./data/'+ endpoint +'.yml'));
   res.render('endpoint', { resource_name: endpoint, endpoints: endpoint_data });
 });
 
-app.use('/', function(req,res){
+app.use('/overview', function(req,res){
   res.render('overview');
 });
 
-app.use('/overview', function(req,res){
-  res.render('overview');
+app.use('/', function(req,res){
+  res.render('index');
 });
 
 var server = app.listen(3000, function() {
